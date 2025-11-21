c4e92b1a3d3855215208f2ed871bc232abaeff34
santhosh1733 <santhoshnagaraj4@gmail.com>
2025-11-21 15:46:49 +0530

    second

---
+++ src/test/java/TestPackage/TestPackageClass2.java
@@ -0,0 +1,8 @@
+package TestPackage;
+
+public class TestPackageClass2 {
+public static void main(String[] args) {
+	System.out.println("test package class 2");
+	System.out.println("test package class 2");
+}
+}
---
+++ src/test/java/TestPackage/testClass3.java
@@ -0,0 +1,7 @@
+package TestPackage;
+
+public class testClass3 {
+public static void main(String[] args) {
+	System.out.println("");
+}
+}
